d664439e1b3f77dbb1e4b2ffbe2bfadab47a3c7f
Dário Batista <dariobat03@gmail.com>
2024-07-05 16:52:20 +0100

    new script locations

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" vol: ", "~/.config/dwm/configs/dwmblocks/sb-volume",	            0,		10},
-	{" | load: ", "sh ~/.config/dwm/configs/dwmblocks/cpu_load.sh",	    60,		0},
-	{" | mem: ", "~/.config/dwm/configs/dwmblocks/sb-memory",	3,		0},
-	{" | ", "date +'%b %d %H:%M;'",					            60,		0},
+	{"", "~/.config/dwm/statusbar/sb-volume",	            0,		10},
+	{" ", "sh ~/.config/dwm/statusbar/cpu_load.sh",	    60,		0},
+	{" ", "~/.config/dwm/statusbar/sb-memory",	3,		0},
+	{" ", "date +'%b %d %H:%M;'",					            60,		0},
     /* Left second bar */
-	{" ", "~/.config/dwm/configs/dwmblocks/sb-battery",	30,		0},
+	{" ", "~/.config/dwm/statusbar/sb-battery",	30,		0},
 	{"", "echo ';'",	                                                        0,		0},
     /* Right second bar */
-	{"", "uname -r ",	0,		0},
-	{" | updates: ", "checkupdates | wc -l",	1800,		0},
-	{" | ", "df | awk -f ~/.config/dwm/configs/dwmblocks/display_disks.awk",	60,		0},
+	{"", "echo [$(uname -r)]",	0,		0},
+	{" ", "echo [$(checkupdates | wc -l)U]",	1800,		0},
+	{" ", "df | awk -f ~/.config/dwm/statusbar/display_disks.awk",	60,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
